45bc176896eda685ccfd98522d9d3e10c2f95373
Yugesherukula0 <yugesherukala@gmail.com>
2025-09-05 07:59:43 +0530

    nullpoinExec

---
+++ javalabs/Lab04092025a.java
@@ -0,0 +1,20 @@
+package javalabs;
+import arrays.SelectionSort;
+public class Lab04092025a {
+    public static void main(String[] args) {
+        int[] arr={21,2,3,54,6};
+        SelectionSort obj= null;
+        try{
+            int[] sorrtedarry=obj.sorting(arr);
+            for(int a:sorrtedarry){
+                System.out.println(a);
+            }
+            
+        }catch(NullPointerException e){
+            System.out.println("cannot call methods by null value as obj is null");
+        }
+        finally{
+            System.out.println("finally");
+        }
+    }
+}
